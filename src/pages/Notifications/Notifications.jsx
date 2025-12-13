22046a02808e327f1a047296a9ddf0238e94f724
Ruslan Teplenko <ruslanteplenko15@gmail.com>
2025-12-13 14:44:04 +0200

    commit

--- src/pages/Notifications/Notifications.jsx
+++ src/pages/Notifications/Notifications.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { useMediaQuery } from "@/shared/ui/UseMedia";
 
-export default function Notifications({post}) {
-    const [notificationId,id,username,avatar,text,name] = post
-
+export default function Notifications({ post }) {
     const Notifications = styled.div`
     background-color: #000;
     padding:15px;
@@ -17,16 +15,24 @@ export default function Notifications({post}) {
     `
 
     return (
-        <Notifications>
-            <NotificationsWrapper>
-                <NotificationsPost>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ducimus nobis reprehenderit debitis provident eligendi quis voluptatum possimus, voluptatem dolor excepturi doloribus incidunt error neque aspernatur perferendis dolore enim obcaecati!</NotificationsPost>
-                <NotificationsPost>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime culpa non rerum similique obcaecati dolorem, dolorum quam temporibus? Eveniet cumque eum ipsum illum aspernatur iusto aut ut aliquam ea. Iste.</NotificationsPost>
-                <NotificationsPost>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ipsum excepturi quos est, harum voluptatibus dicta ratione quasi illo sed consectetur inventore molestias neque quibusdam! Fugiat optio dolor placeat magnam.</NotificationsPost>
-                <NotificationsPost>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem in pariatur ullam perspiciatis nulla, sunt recusandae suscipit deleniti obcaecati incidunt omnis est sapiente id, dignissimos veritatis qui blanditiis, doloremque voluptatem.</NotificationsPost>
-                <NotificationsPost>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi maiores aperiam quia. Eum tempora dicta natus pariatur, porro voluptas ut ad quam velit molestias maiores numquam quaerat! Explicabo, reiciendis dolorum.</NotificationsPost>
-            </NotificationsWrapper>
-        </Notifications>
+        <>
 
+
+            {post.map((singlePost) => (
+                console.log(singlePost)
+            ))}
+
+            <Notifications>
+                <NotificationsWrapper>
+                    <NotificationsPost>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ducimus nobis reprehenderit debitis provident eligendi quis voluptatum possimus, voluptatem dolor excepturi doloribus incidunt error neque aspernatur perferendis dolore enim obcaecati!</NotificationsPost>
+                    <NotificationsPost>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime culpa non rerum similique obcaecati dolorem, dolorum quam temporibus? Eveniet cumque eum ipsum illum aspernatur iusto aut ut aliquam ea. Iste.</NotificationsPost>
+                    <NotificationsPost>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ipsum excepturi quos est, harum voluptatibus dicta ratione quasi illo sed consectetur inventore molestias neque quibusdam! Fugiat optio dolor placeat magnam.</NotificationsPost>
+                    <NotificationsPost>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem in pariatur ullam perspiciatis nulla, sunt recusandae suscipit deleniti obcaecati incidunt omnis est sapiente id, dignissimos veritatis qui blanditiis, doloremque voluptatem.</NotificationsPost>
+                    <NotificationsPost>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi maiores aperiam quia. Eum tempora dicta natus pariatur, porro voluptas ut ad quam velit molestias maiores numquam quaerat! Explicabo, reiciendis dolorum.</NotificationsPost>
+                </NotificationsWrapper>
+            </Notifications>
+
+        </>
 
     )
 
